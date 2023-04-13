c13694a22408a1f39b47e843b4cb86348d2898ec
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-13 16:58:31 +0200

    delete function good

--- supression.php
+++ supression.php
@@ -1,6 +1,8 @@
 <?php
     session_start();    
     
-    echo ($_POST('submit')); 
-
+    echo $_POST['submit']; 
+    unset($_SESSION['products'][$_POST['submit']]);
+    header("Location:recap.php");
 ?>
+
