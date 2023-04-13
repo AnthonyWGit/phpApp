6f878c7817d27260041ab4ec453229f98ae32b61
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-13 14:38:28 +0200

    setup conditions

--- traitement.php
+++ traitement.php
@@ -5,6 +5,7 @@
         $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);                                   //Vérifier que toutes les valeurs correspondent à ce qu'on veut
         $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
         $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+        $_SESSION['validMsg'] = "Le produit a été entré";
         if($name && $price && $qtt)
         {
             $product = [                                         //Caractéristiques d'un produit
@@ -14,6 +15,10 @@
                 "total" => $price*$qtt
             ]; 
             $_SESSION['products'][] = $product;                 //Mettre le production dans un array de produits conservé dans la superglobale session
+        }
+        else
+        {
+            $_SESSION['errorMsg'] = "Juste non";
         }
     }   
 header("Location:index.php");           //Toujours à la fin ! Redirection si utilisateur arrive sur cette page via url 
