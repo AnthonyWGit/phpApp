4ca552db2e966c8c8efcea0f60410a0e14ce6ac6
Anthony <anthony.wetzstein@gmail.com>
2023-04-18 15:41:38 +0200

    Products can have pictures

--- traitement.php
+++ traitement.php
@@ -19,20 +19,29 @@
                     {
                         var_dump($_FILES);
                          $target_dir = "uploads/";                                                   //Emplacement où les images sont hébergées
-                         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);     // .basename retourne la dernière composante du chemin
+                         $target_file = $target_dir . basename($_FILES["fileToUpload"]["tmp_name"]);     // .basename retourne la dernière composante du chemin
                          $uploadOk = 1;
                          $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));     //cherche le type du fichier
                          $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
                          if($check !== false) 
-                         {
-                         echo "Fichier est bien une image - " . $check["mime"] . ".";
-                         $uploadOk = 1;
-                         } 
-                         else 
-                         {
-                         echo "Fichier N4EST PAS UNE IMAGe.";
-                         $uploadOk = 0;
-                         }  
+                        {
+                           echo "Fichier est bien une image - " . $check["mime"] . ".";
+                           $uploadOk = 1;
+                           var_dump($check["mime"]);
+                        } 
+                        else 
+                        {
+                           echo "Fichier N4EST PAS UNE IMAGe.";
+                           $uploadOk = 0;
+                        }
+                        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+                        {
+                            echo "Le fichier ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " a été téléchargé.";
+                        } else 
+                        {
+                            echo "Non, ça ne marche pas d'upload un pdf.";
+                        }
+                        echo "<img src='".$target_file."'>";
                     }
                     else
                     {
@@ -49,7 +58,8 @@
                             "name"  => $name,
                             "price" => $price,
                             "qtt"   => $qtt,
-                            "total" => $price*$qtt
+                            "total" => $price*$qtt,
+                            "image" => $target_file
                         ]; 
                         $_SESSION['products'][] = $product;                 //Mettre le production dans un array de produits conservé dans la superglobale session
                         var_dump($_SESSION['products']);
