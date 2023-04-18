d0d7dd279bf26d144e36f4633d277cfebe3f873b
Anthony <anthony.wetzstein@gmail.com>
2023-04-18 16:25:03 +0200

    working modal

--- recap.php
+++ recap.php
@@ -75,15 +75,15 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Aperçu du produit</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <p> Modale </p>
+        <?= '<img src="'. $_SESSION["products"][$index]["image"]. '">'  ?>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-warning" data-bs-dismiss="modal"> Fermer </button>
       </div>
     </div>
   </div>
